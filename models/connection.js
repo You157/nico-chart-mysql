7a624747c8676d47168b2ecbd7039c438c30110f
You157 <youshimo26@gmail.com>
2020-11-23 23:47:49 +0900

    mysqlへのconnectテスト

--- models/connection.js
+++ models/connection.js
@@ -2,10 +2,16 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host:'localhost',
-    user:'yoush',
-    password: 'mysql',
-    database:'nico_chart'
+  /*
+  host:'localhost',
+  user:'yoush',
+  password: 'mysql',
+  database:'nico_chart'
+  */
+  host:'us-cdbr-east-02.cleardb.com',
+  user:'beca2f0462ec18',
+  password: '835ade6e',
+  database:'heroku_0231a46a5335cc6'
 });
 
 connection.connect((err)=> {
