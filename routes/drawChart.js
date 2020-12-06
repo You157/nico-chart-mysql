d27130396b3aba5a57939a191cb914db037c30a0
You157 <youshimo26@gmail.com>
2020-12-06 22:32:27 +0900

    test用のLog出力を追記しました。

--- routes/drawChart.js
+++ routes/drawChart.js
@@ -15,6 +15,7 @@ router.get('/', function (req, res, next) {
   let commentsCount;
   switch (params) {
     case '0':
+      console.log('case:0');
       var sql = `(select * from total where date like '%-12-31') 
         union (select * from total order by date desc limit 1)`;
       connection.query(sql, function (err, result) {
@@ -38,6 +39,7 @@ router.get('/', function (req, res, next) {
       });
       break;
     case '1':
+      console.log('case:1');
       var sql = `
         select * from total where date in (
           '${year - 1}-12-31', '${year}-01-31', last_day('${year}-02-01'), '${year}-03-31', '${year}-04-30', 
@@ -67,6 +69,7 @@ router.get('/', function (req, res, next) {
       });
       break;
     case '2':
+      console.log('case:2');
       var sql = `
         select * from total 
           where date like '${year}-${month}%'
